de5b80666b1de23f85bcdc42be6d0dc72f6fbf5f
Sudip-Magar <sudeepmagar730@gmail.com>
2025-11-26 10:26:03 +0545

    limit added on notification component call

--- app/Livewire/Menus/Notification.php
+++ app/Livewire/Menus/Notification.php
@@ -10,12 +10,23 @@ use App\Models\Notification as modelNotification;
 
 class Notification extends Component
 {
-    
+    public $limit = null;
+
+    public function mount($limit = null)
+    {
+        $this->limit = $limit;
+    }
+
     public function render()
     {
-        $user= Auth::guard('web')->user();
+        $user = Auth::guard('web')->user();
+        $query = modelNotification::where('user_id', $user->id)->latest();
+        if ($this->limit) {
+            $query->limit($this->limit);
+        }
+
         return view('livewire.menus.notification', [
-            'notifications' => modelNotification::where('user_id',$user->id)->latest()->limit(10)->get(),
+            'notifications' => $query->get(),
         ]);
     }
 }
